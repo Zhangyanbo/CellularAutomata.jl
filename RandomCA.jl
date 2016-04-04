c583599e0ba25511b4120a380d56889b3b52e1d6
Zhang Yanbo <zhangyb1@mail.ustc.edu.cn>
2016-04-04 22:51:39 +0800

    数值上的修改

--- RandomCA.jl
+++ RandomCA.jl
@@ -1,5 +1,5 @@
 include("CellularAutomata.jl")
 
-rCA = CARunNTimes(110, [rand()<0.5?0:1 for i = 1:300], 300)
+rCA = CARunNTimes(110, [rand()<0.5?0:1 for i = 1:1000], 1000)
 
 writecsv("E:\\GitHub\\CellularAutomata\\ca_out.csv",rCA)
