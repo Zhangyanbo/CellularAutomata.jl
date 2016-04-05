db86c4f11e076b3d6ece3de61df601867edc4878
Zhang Yanbo <zhangyb1@mail.ustc.edu.cn>
2016-04-05 09:39:43 +0800

    增加注释

--- RandomCA.jl
+++ RandomCA.jl
@@ -3,3 +3,5 @@ include("CellularAutomata.jl")
 rCA = CARunNTimes(110, [rand()<0.5?0:1 for i = 1:1000], 1000)
 
 writecsv("E:\\GitHub\\CellularAutomata\\ca_out.csv",rCA)
+
+# draw image in Mathematica by running: ArrayPlot[Import["E:\\GitHub\\CellularAutomata\\ca_out.csv", "CSV"]]
